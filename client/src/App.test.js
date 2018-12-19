e90e98e65feb1e86497d5058caaaebf5481960ff
Aaron Mednick <aaronjosephm@gmail.com>
2018-12-19 14:33:11 -0800

    first commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 import ReactDOM from 'react-dom';
-import App from './App';
+import Form from './form';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('Check to see that form can respond to enter key', () => {
+	const form = shallow(
+		<Form />
+		);
+	expect()
 });
